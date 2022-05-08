2dcef9110eed5663c0a322281f372be24229a9b5
TheLostLegend <arhir.13.07.2002@gmail.com>
2022-05-08 14:57:22 +0300

    Lab4 initial

---
+++ src/ui.js
@@ -0,0 +1,17 @@
+export const loginUsername = document.querySelector('#loginUsername')
+export const loginPassword = document.querySelector('#loginPassword')
+
+export const sigupUsername = document.querySelector('#sigupUsername')
+export const sigupPassword = document.querySelector('#sigupPassword')
+
+export const btnLogin = document.querySelector('#btnLogin')
+export const btnSignup = document.querySelector('#btnSignup')
+
+export const btnLoginForm = document.querySelector('#btnLoginForm')
+export const btnSignupForm = document.querySelector('#btnSignupForm')
+
+
+export const btnLogout = document.querySelector('#btnLogout')
+
+
+export const navPanel = document.querySelector('#NavPan')
